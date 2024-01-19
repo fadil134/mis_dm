7cfa84c559fc45860fe28fe2b64415b8d56c3aaf
fadil134 <130072912+fadil134@users.noreply.github.com>
2024-01-19 19:43:36 +0700

    modified:   application/views/dist/features-post-create.php

--- application/views/dist/features-post-create.php
+++ application/views/dist/features-post-create.php
@@ -42,7 +42,7 @@ $this->load->view('dist/_partials/header');
               <div class="form-group row mb-4">
                 <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
                 <div class="col-sm-12 col-md-7">
-                  <div id="summernote" style="z-index: 1041;"></div>
+                  <div id="summernote"></div>
                 </div>
               </div>
               <div class="form-group row mb-4">
