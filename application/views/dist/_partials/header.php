c0ce32c72f9ba76ef1d72cf48584db2bbe01d913
fadil134 <130072912+fadil134@users.noreply.github.com>
2024-02-12 01:44:07 +0700

    header.php modifications

--- application/views/dist/_partials/header.php
+++ application/views/dist/_partials/header.php
@@ -153,7 +153,7 @@ if ($this->uri->segment(2) == "layout_transparent") {
 } elseif ($this->uri->segment(2) == "layout_top_navigation") {
     $this->load->view('dist/_partials/layout-3');
     $this->load->view('dist/_partials/navbar');
-} elseif ($this->uri->segment(2) != "auth_login" && $this->uri->segment(2) != "auth_forgot_password" && $this->uri->segment(2) != "auth_register" && $this->uri->segment(2) != "auth_reset_password" && $this->uri->segment(2) != "errors_503" && $this->uri->segment(2) != "errors_403" && $this->uri->segment(2) != "errors_404" && $this->uri->segment(2) != "errors_500" && $this->uri->segment(2) != "utilities_contact" && $this->uri->segment(2) != "masuk" && $this->uri->segment(2) != "s_galeri" && $this->uri->segment(2) != "u_galeri" && $this->uri->segment(2) != "utilities_subscribe" && $this->uri->segment(1) != "Kmberanda") {
+} elseif ($this->uri->segment(2) != "auth_login" && $this->uri->segment(2) != "auth_forgot_password" && $this->uri->segment(2) != "auth_register" && $this->uri->segment(2) != "auth_reset_password" && $this->uri->segment(2) != "errors_503" && $this->uri->segment(2) != "errors_403" && $this->uri->segment(2) != "errors_404" && $this->uri->segment(2) != "errors_500" && $this->uri->segment(2) != "utilities_contact" && $this->uri->segment(2) != "masuk" && $this->uri->segment(2) != "s_galeri" && $this->uri->segment(2) != "daftar" && $this->uri->segment(2) != "u_galeri" && $this->uri->segment(2) != "utilities_subscribe" && $this->uri->segment(1) != "Kmberanda") {
     $this->load->view('dist/_partials/layout');
     $this->load->view('dist/_partials/sidebar');
 }
